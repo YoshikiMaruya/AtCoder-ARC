e660d3d55fd33a3280b975c444ffe4777d39859f
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-07-22 19:11:32 +0900

    arc122

---
+++ arc122/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define reps(i, s, n) for (int i = (s); i < (n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+#define MOD 1000000007
+
+int main(){
+  ll n,a[100005];
+
+  cin>>n;
+  rep(i,n) cin>>a[i];
+
+  
+}
